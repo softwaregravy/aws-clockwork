2336aee47c426361a6f7f6fcb40816730bba8a42
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-09 23:54:37 -0400

    unused models

--- spec/factories/aws_tickwork_active_record_data_stores.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :aws_tickwork_active_record_data_store, class: 'AwsTickwork::ActiveRecordDataStore' do
-    key "MyString"
-    value 1
-  end
-end
--- spec/factories/aws_tickwork_d_b_data_stores.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :aws_tickwork_d_b_data_store, class: 'AwsTickwork::DBDataStore' do
-    key "MyString"
-    value 1
-  end
-end
